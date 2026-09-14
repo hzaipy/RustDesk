574b469487e68b0f744cfef755026a1ecad84d4b
hzaipy <56548926+hzaipy@users.noreply.github.com>
2026-09-14 14:31:51 +0800

    chore: bake custom config into common.dart

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -2984,7 +2984,7 @@ class ServerConfig {
   ServerConfig.fromOptions(Map<String, dynamic> options)
       : idServer = options['custom-rendezvous-server'] ?? "",
         relayServer = options['relay-server'] ?? "",
-        apiServer = options['api-server'] ?? "",
+        apiServer = options['api-server'] ?? "https://rds.ioslm.cn",
         key = options['key'] ?? "";
 }
 
